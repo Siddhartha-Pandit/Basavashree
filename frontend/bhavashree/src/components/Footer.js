a771fbb44e7ae66a2e7c311fe7e5d055cb44ed4c
Siddhartha <siddharthapandit66@gmail.com>
2024-08-01 21:19:17 +0530

    09.13PM 2024.08.01 added footer component

--- frontend/bhavashree/src/components/Footer.js
+++ frontend/bhavashree/src/components/Footer.js
@@ -1,5 +1,54 @@
+import React from "react";
+
 const Footer = () => {
-  return <></>;
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-left">
+          <div className="footer-description">
+            We are serving the people of Karnataka through the Souharda Sahakari
+            movement. We have been working in the field of finance, social,
+            cultural sports, religious and health, women and youth empowerment,
+            and so on.
+          </div>
+          <div className="footer-logo">
+            <i class="fas fa-map-marker-alt map-icon"></i>
+            Basavashree Souharda Co-operative Society Ltd.
+            <br />
+            Office: A/P. Zalaki, Chadchan road, tal. Indi,
+            <br />
+            Dist. Vijaypura, Karnataka
+          </div>
+        </div>
+        <div className="footer-right">
+          <div className="social-icons">
+            <a href="#">
+              <i class="fab fa-facebook"></i>
+            </a>
+            <a href="#">
+              <i class="fab fa-instagram"></i>
+            </a>
+            <a href="#">
+              <i class="fab fa-twitter"></i>
+            </a>
+          </div>
+          <div className="footer-links">
+            <a href="index.html">Home</a>
+            <a href="about.html">About Us</a>
+            <a href="index.html">Services</a>
+            <a href="contact.html">Contact</a>
+            <a href="personal_loan.html">Personal Loan</a>
+            <a href="gold_loan.html">Gold Loan</a>
+            <a href="vehicle_loan.html">Vehicle Loan</a>
+            <a href="mortage_loan.html">Mortgage Loan</a>
+          </div>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        &copy; 2024 Basavashree Souharda Co-operative Society Ltd.
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
